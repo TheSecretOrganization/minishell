0ce4058df52f790756c77b0c2a54ad045deec1b2
Andrei Basdereff <114996380+Neffi42@users.noreply.github.com>
2024-01-08 16:07:50 +0100

    miniquoi ?

--- src/main.c
+++ src/main.c
@@ -3,18 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:18:12 by averin            #+#    #+#             */
-/*   Updated: 2024/01/04 10:32:28 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/08 15:05:16 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char const *argv[])
+int	main(void)
 {
-	(void)argc;
-	(void)argv;
+	char	*line;
+
+	line = readline("miniquoi? ");
+	add_history(line);
+	printf("%s\n", line);
+	free(line);
 	return (0);
 }
