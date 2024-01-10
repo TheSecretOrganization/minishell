333df7d545780b8bbd822dcd59e13c579cc57533
Andrei Basdereff <114996380+Neffi42@users.noreply.github.com>
2024-01-10 16:21:44 +0100

    added call to parse_line

--- src/main.c
+++ src/main.c
@@ -3,22 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:18:12 by averin            #+#    #+#             */
-/*   Updated: 2024/01/10 15:35:08 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/10 16:13:47 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parsing.h"
 
 int	main(void)
 {
 	char	*line;
+	t_cmd	cmd;
 
 	line = NULL;
+	cmd.args = NULL;
+	cmd.elements = NULL;
 	register_signals();
 	while (prompt(&line))
-		;
+	{
+		if (!parse_line(&cmd, line))
+			return (free(line), -1);
+	}
 	return (0);
 }
