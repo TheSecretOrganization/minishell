dc17ba2c7976d5cf1e6aae2b3f756191bc44c40a
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 12:34:00 +0100

    created t_token_size function

--- src/parsing/t_token/t_token_size.c
+++ src/parsing/t_token/t_token_size.c
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   t_token_size.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:03:15 by abasdere         ###   ########.fr       */
+/*   Created: 2024/01/11 11:47:21 by abasdere          #+#    #+#             */
+/*   Updated: 2024/01/11 12:33:13 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#include "parsing.h"
 
-# include "minishell.h"
-
-typedef struct s_token
+/**
+ * @return size_t of lst
+*/
+size_t	t_token_size(t_token *lst)
 {
-	int		val;
-	char	*id;
-	t_token	*next;
-	t_token	*prev;
-}	t_token;
+	size_t	i;
 
-t_token	*t_token_last(t_token *lst);
-size_t	t_token_size(t_token *lst);
-
-t_bool	parse_line(t_cmd *cmd, char *line);
-
-#endif
+	i = 1;
+	if (!lst)
+		return (0);
+	while (lst->next && i++)
+		lst = lst->next;
+	return (i);
+}
