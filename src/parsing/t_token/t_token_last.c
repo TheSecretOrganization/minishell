6ed159e2e2b1ebb9fe6080b17d880d1f8af28244
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 12:43:18 +0100

    updated documentation of t_token_size and t_token_last

--- src/parsing/t_token/t_token_last.c
+++ src/parsing/t_token/t_token_last.c
@@ -6,14 +6,15 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:55:53 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:38:19 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/11 12:42:26 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
 /**
- * Find the last node of lst
+ * Find the last node of a t_token list
+ * @param lst head of the list
  * @return t_token * or NULL if lst is NULL
 */
 t_token	*t_token_last(t_token *lst)
--- src/parsing/t_token/t_token_size.c
+++ src/parsing/t_token/t_token_size.c
@@ -6,14 +6,15 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:47:21 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:38:40 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/11 12:42:48 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
 /**
- * Calculate the size of lst
+ * Calculate the size of a t_token list
+ * @param lst head of the list
  * @return size_t or 0 is lst is NULL
 */
 size_t	t_token_size(t_token *lst)
