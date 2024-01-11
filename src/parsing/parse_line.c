50ac8df498b0641680bb772cc12ee3deeb13acf7
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 19:57:07 +0100

    create init_spcl_chars function

--- src/parsing/parse_line.c
+++ src/parsing/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:08:16 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 19:34:05 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/11 19:55:05 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,33 @@ static t_bool	init_tokens(t_token **lst, char *line)
 	return (B_TRUE);
 }
 
+ * Assign the special char tokens their final values
+ * @param lst head of the list
+ * @return t_bool return B_TRUE or B_FALSE if there is unclosed (double) quotes
+*/
+static t_bool	init_spcl_chars(t_token *lst)
+{
+	size_t	nb[2];
+
+	nb[0] = 0;
+	nb[1] = 0;
+	while (lst)
+	{
+		if (nb[0] % 2 && lst->val != CHAR && lst->val != QUOTE)
+			lst->val = CHAR;
+		if (nb[1] % 2 && lst->val != CHAR && lst->val != D_QUOTE && \
+		lst->val != VARIABLE)
+			lst->val = CHAR;
+		if ((lst->val == QUOTE && ++(nb[0])) || \
+		(lst->val == D_QUOTE && ++(nb[1])))
+			;
+		lst = lst->next;
+	}
+	if (nb[0] % 2 || nb[1] % 2)
+		return (B_FALSE);
+	return (B_TRUE);
+}
+
 /**
  * Parse user's input
  * @return B_TRUE if user's input is valid, B_FALSE if it isn't
@@ -42,7 +69,7 @@ t_bool	parse_line(t_cmd *cmd, char *line)
 	t_token	*lst;
 
 	lst = NULL;
-	if (!init_tokens(&lst, line))
-		return (B_FALSE);
+	if (!init_tokens(&lst, line) || !init_spcl_chars(lst))
+		return (t_token_clear(&lst), B_FALSE);
 	return (t_token_clear(&lst), B_TRUE);
 }
