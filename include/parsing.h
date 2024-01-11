93ee6326ad5258a62f1ae285266868100b29bd9e
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 13:47:00 +0100

    created t_token_new function

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:03:15 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/11 13:44:42 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,21 @@
 
 # include "minishell.h"
 
+typedef enum e_val
+{
+	ERROR = 0,
+	CHAR = 1,
+	SEP = 2,
+	WILDCARD = 3,
+	VARIABLE = 4,
+	QUOTE = 5,
+	D_QUOTE = 6,
+	SPCL_CHAR = 7
+}	t_val;
+
 typedef struct s_token
 {
-	int		val;
+	t_val	val;
 	char	*id;
 	t_token	*next;
 	t_token	*prev;
@@ -27,6 +39,7 @@ void	t_token_addback(t_token **lst, t_token *new);
 void	t_token_clear(t_token **lst);
 void	t_token_delone(t_token *lst);
 t_token	*t_token_last(t_token *lst);
+t_token	*t_token_new(char *id);
 size_t	t_token_size(t_token *lst);
 
 t_bool	parse_line(t_cmd *cmd, char *line);
---
+++ src/parsing/t_token/t_token_new.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_token_new.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 11:49:27 by abasdere          #+#    #+#             */
+/*   Updated: 2024/01/11 13:46:11 by abasdere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+/**
+ * Find the token value of a char
+ * @param c char to tokenized
+ * @return t_val
+*/
+static t_val	get_val(char c)
+{
+	if (ft_strchr("~`#()\\[]{};?!", c))
+		return (ERROR);
+	if (ft_strchr("&|<>", c))
+		return (SPCL_CHAR);
+	else if (c == '\"')
+		return (D_QUOTE);
+	else if (c =='\'')
+		return (QUOTE);
+	else if (c == '$')
+		return (VARIABLE);
+	else if (c == '*')
+		return (WILDCARD);
+	else if (c == ' ')
+		return (SEP);
+	return (CHAR);
+}
+
+/**
+ * Create a new t_token node
+ * @param id value to tokenized
+ * @return t_token	* or NULL if an error was encountered
+*/
+t_token	*t_token_new(char *id)
+{
+	t_token	*new;
+
+	new = ft_calloc(1, sizeof(t_token));
+	if (!new)
+		return (NULL);
+	new->val = get_val(*id);
+	new->id = id;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
