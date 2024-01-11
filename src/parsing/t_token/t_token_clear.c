a421891413f4eeb94c386ec67d06b3ebed5be702
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 12:47:30 +0100

    created t_token_clear function

--- src/parsing/t_token/t_token_clear.c
+++ src/parsing/t_token/t_token_clear.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   t_token_clear.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:03:15 by abasdere         ###   ########.fr       */
+/*   Created: 2024/01/11 11:59:18 by abasdere          #+#    #+#             */
+/*   Updated: 2024/01/11 12:45:36 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#include "parsing.h"
 
-# include "minishell.h"
 
-typedef struct s_token
+/**
+ * Delete a t_token list
+ * @param lst node to destroy
+*/
+void	t_token_clear(t_token **lst)
 {
-	int		val;
-	char	*id;
-	t_token	*next;
-	t_token	*prev;
-}	t_token;
-
-void	t_token_clear(t_token **lst);
-void	t_token_delone(t_token *lst);
-t_token	*t_token_last(t_token *lst);
-size_t	t_token_size(t_token *lst);
-
-t_bool	parse_line(t_cmd *cmd, char *line);
-
-#endif
+	if (!lst || !(*lst))
+		return ;
+	if ((*lst)->next)
+		t_token_clear(&(*lst)->next);
+	t_token_delone(*lst);
+	*lst = NULL;
+}
