5b91dfb78caf1475321baa1d15b01b30e6a5974f
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-11 12:46:51 +0100

    updated documentation of t_token_delone

--- src/parsing/t_token/t_token_delone.c
+++ src/parsing/t_token/t_token_delone.c
@@ -6,14 +6,14 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:58:31 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/11 12:41:35 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/11 12:45:28 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
 /**
- * Delete one t_token node
+ * Delete a t_token node
  * @param lst node to destroy
 */
 void	t_token_delone(t_token *lst)
