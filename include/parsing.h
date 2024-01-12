5554863480092e9ef44244488ac4d3fff5cf09d8
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-13 00:36:26 +0100

    pass to iterative tokens rather than chain listed

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/12 19:29:29 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/13 00:13:57 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,22 @@ typedef enum e_val
 	V_ERROR = 0,
 	V_SEP = 1,
 	V_CHAR = 2,
-	V_WILDCARD = 3,
-	V_VARIABLE = 4,
+	V_WLDCRD = 3,
+	V_VAR = 4,
 	V_QUOTE = 5,
 	V_DQUOTE = 6,
-	V_SPCL_CHAR = 7
+	V_REDIR = 7,
+	V_OPE = 8
 }	t_val;
 
 typedef struct s_token
 {
-	t_val			val;
-	char			*id;
-	struct s_token	*next;
-	struct s_token	*prev;
+	char	*id;
+	t_val	val;
 }	t_token;
 
-void	t_token_addback(t_token **tk, t_token *new);
-void	t_token_clear(t_token **tk);
-t_token	*t_token_last(t_token *tk);
-t_token	*t_token_new(char *id);
-void	t_token_print_vals(t_token *tk);
-size_t	t_token_size(t_token *tk);
-
-t_code	clean_memory(t_code code, t_cmd *cmd, char *line, t_token **tk);
+t_code	clean_memory(t_code code, t_cmd *cmd, char *line, t_token *tk);
 t_code	error_syntax(t_code code, char el);
 t_code	parse_line(t_cmd **cmd, char *line);
-t_token	*parse_spcl_char(t_code *code, t_cmd **cmd, t_token *tk);
 
 #endif
--- src/parsing/parse_line.c
+++ src/parsing/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:08:16 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/12 19:39:07 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/13 00:35:28 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,64 @@
 
 /**
  * Tokenize a line
- * @param tk pointer to the head of the list
  * @param line line to tokenize
- * @return t_code return C_SUCCES or C_ERR_MEM if malloc fails
-*/
-static t_code	init_tokens(t_token **tk, char *line)
+ * @param tk tokens to init
+ * @param i counter
+ * @return t_code C_SUCCES
+ * */
+static t_code	init_tokens(char *line, t_token *tk, size_t i)
 {
-	t_token	*tmp;
-
-	tmp = NULL;
-	while (*line)
+	while (line[++i])
 	{
-		tmp = t_token_new(line++);
-		if (!tmp)
-			return (t_token_clear(tk), \
-			error(C_ERR_MEM, "t_token_new", ERR_MEM));
-		t_token_addback(tk, tmp);
+		tk[i].id = &line[i];
+		if (ft_strchr("~`#()\\[]{};?!", line[i]))
+			tk[i].val = V_ERROR;
+		else if (ft_strchr("&|", line[i]))
+			tk[i].val = V_OPE;
+		else if (ft_strchr("<>", line[i]))
+			tk[i].val = V_REDIR;
+		else if (line[i] == '\"')
+			tk[i].val = V_DQUOTE;
+		else if (line[i] == '\'')
+			tk[i].val = V_QUOTE;
+		else if (line[i] == '$')
+			tk[i].val = V_VAR;
+		else if (line[i] == '*')
+			tk[i].val = V_WLDCRD;
+		else if (line[i] == ' ')
+			tk[i].val = V_SEP;
+		else
+			tk[i].val = V_CHAR;
 	}
+	tk[i].id = NULL;
+	tk[i].val = -1;
 	return (C_SUCCES);
 }
 
 /**
- * Assign the special char tokens their final values
- * @param tk head of the list
- * @return t_code return C_SUCCES or C_BAD_USAGE if there is unclosed quotes
+ * Assign the tokens their final values
+ * @param tk tokens to check
+ * @param i counter
+ * @return t_code C_SUCCES or C_BAD_USAGE
 */
-static t_code	init_spcl_chars(t_token *tk)
+static t_code	check_tokens(t_token *tk, size_t i)
 {
 	size_t	nb[2];
 
 	nb[0] = 0;
 	nb[1] = 0;
-	while (tk)
+	while (tk[++i].id)
 	{
-		if (!(nb[0] % 2) && !(nb[1] % 2) && tk->val == V_ERROR)
-			return (error_syntax(C_BAD_USE, *(tk->id)));
-		if (nb[0] % 2 && tk->val != V_CHAR && tk->val != V_QUOTE)
-			tk->val = V_CHAR;
-		if (nb[1] % 2 && tk->val != V_CHAR && tk->val != V_DQUOTE && \
-		tk->val != V_VARIABLE)
-			tk->val = V_CHAR;
-		if ((tk->val == V_QUOTE && ++(nb[0])) || \
-		(tk->val == V_DQUOTE && ++(nb[1])))
+		if (!(nb[0] % 2) && !(nb[1] % 2) && tk[i].val == V_ERROR)
+			return (error_syntax(C_BAD_USE, *tk[i].id));
+		else if ((nb[0] % 2 && tk[i].val != V_QUOTE) || \
+		(nb[1] % 2 && tk[i].val != V_DQUOTE && tk[i].val != V_VAR))
+			tk[i].val = V_CHAR;
+		else if (tk[i].val == V_VAR && tk[i + 1].id && tk[i + 1].val == V_CHAR)
+			tk[i + 1].val = V_VAR;
+		if ((tk[i].val == V_QUOTE && ++(nb[0])) || \
+		(tk[i].val == V_DQUOTE && ++(nb[1])))
 			;
-		if (tk->val == V_VARIABLE && tk->next && tk->next->val == V_CHAR)
-			tk->next->val = V_VARIABLE;
-		tk = tk->next;
 	}
 	if (nb[0] % 2)
 		return (error_syntax(C_BAD_USE, '\''));
@@ -69,82 +81,25 @@ static t_code	init_spcl_chars(t_token *tk)
 }
 
 /**
- * Parse in function of their precedence
- * @param code return code
- * @param list of comamnd to init
- * @param tk head of the token list to parse
- * @param prec determine what level of tokens is being treated
- * @return t_token new position in the token list or NULL if a error occurs
-*/
-t_token	*parse_tokens(t_code *code, t_cmd **cmd, t_token *tk, t_val prec)
-{
-	if (prec == V_SEP)
-		while (tk && tk->val == V_SEP)
-			tk = tk->next;
-	else if (prec == V_CHAR)
-		;
-	else if (prec == V_WILDCARD)
-		return (error_syntax(C_BAD_USE, '*'), NULL);
-	else if (prec == V_VARIABLE)
-		;
-	else if (prec == V_QUOTE)
-		;
-	else if (prec == V_DQUOTE)
-		;
-	else if (prec == V_SPCL_CHAR)
-		return (parse_spcl_char(code, cmd, tk));
-	return (tk);
-}
-
-/**
- * Use the precedence climbing algorithm to parse the tokens
- * @param code return code
- * @param list of comamnd to init
- * @param tk head of the token list to parse
- * @param prec determine what level of tokens is being treated
- * @return t_token new position in the token list or NULL if a error occurs
-*/
-static t_token	*prec_climb(t_code *code, t_cmd **cmd, t_token *tk, t_val prec)
-{
-	return (tk);
-	while (tk && tk->val >= prec)
-	{
-		if (prec != V_SPCL_CHAR && tk->val == prec)
-		{
-			prec_climb(code, cmd, tk->next, prec + 1);
-			if (code != C_SUCCES)
-				return (NULL);
-		}
-		if (tk->val > prec)
-			tk = prec_climb(code, cmd, tk, prec + 1);
-		if (!tk && *code != C_SUCCES)
-			return (NULL);
-		tk = parse_tokens(code, cmd, tk, prec);
-		if (!tk && *code != C_SUCCES)
-			return (NULL);
-	}
-	return (tk);
-}
-
-/**
  * Parse user's input, exit the program if error occurs
  * @param cmd list of comamnd to init
- * @param line pointer to the line to parse
+ * @param line line to parse
  * @return t_code C_SUCCES or an error code
 */
 t_code	parse_line(t_cmd **cmd, char *line)
 {
 	t_token	*tk;
-	t_code	code;
+	size_t	i;
 
-	tk = NULL;
-	code = C_SUCCES;
-	if (init_tokens(&tk, line))
-		exit(clean_memory(C_ERR_MEM, *cmd, line, &tk));
-	if (init_spcl_chars(tk))
-		return (clean_memory(C_BAD_USE, *cmd, NULL, &tk));
-	prec_climb(&code, cmd, tk, V_SEP);
-	if (code != C_SUCCES)
-		exit(clean_memory(C_ERR_MEM, *cmd, line, &tk));
-	return (clean_memory(code, *cmd, NULL, &tk));
+	printf("%s\n", line);
+	tk = malloc((ft_strlen(line) + 1) * sizeof(t_token));
+	if (!tk)
+		exit(clean_memory(C_ERR_MEM, *cmd, line, tk));
+	if (!init_tokens(line, tk, -1) && check_tokens(tk, -1))
+		return (clean_memory(C_BAD_USE, *cmd, NULL, tk));
+	i = -1;
+	while (tk[++i].id)
+		printf("%d", tk[i].val);
+	printf("\n");
+	return (clean_memory(C_SUCCES, *cmd, NULL, tk));
 }
