d679034adb046756a4eee914b822b10e630e2d73
averin <averin@student.42.fr>
2024-01-15 12:39:03 +0100

    find exec in path #29

--- include/execution.h
+++ include/execution.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 10:17:31 by averin            #+#    #+#             */
-/*   Updated: 2024/01/15 11:06:44 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/15 12:13:07 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define EXECUTION_H
 
 # include "minishell.h"
+# include "errno.h"
+
+# define E_NOEXEC 1
+# define E_NOFILE 2
+# define E_MEM 3
 
 int	exec_cmd(t_cmd *cmd, char **path);
 int	do_exec(char *exec, char **args, char **envp, int *fds);
--- src/execution/exec.c
+++ src/execution/exec.c
@@ -6,12 +6,41 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 10:41:06 by averin            #+#    #+#             */
-/*   Updated: 2024/01/15 11:00:54 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/15 12:13:01 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "execution.h"
+
+/**
+ * Find an executable in the path
+ * @param cmd command name
+ * @param path list of path entries
+ * @return path or NULL
+*/
+static char	*find_path_exec(char *cmd, char **path)
+{
+	char	*exec;
+	size_t	i;
+
+	i = -1;
+	while (path[++i])
+	{
+		exec = ft_strjoin(cmd, path[i]);
+		if (!exec)
+			return (errno = E_MEM, NULL);
+		if (access(exec, F_OK) == -1)
+		{
+			free(exec);
+			continue ;
+		}
+		if (access(exec, F_OK | X_OK) == -1)
+			return (free(exec), errno = E_NOEXEC, NULL);
+		return (exec);
+	}
+	return (free(exec), errno = E_NOFILE, NULL);
+}
 
 /**
  * Execute a command
@@ -21,7 +50,6 @@
 */
 int	exec_cmd(t_cmd *cmd, char **path)
 {
-	int	pid;
 
 	pid = do_exec(exec, cmd->args, NULL, (int []){0, 1});
 	waitpid(pid, NULL, 0);
