dab5aa8685a31cd5a96564264942a22fe9a44faa
averin <averin@student.42.fr>
2024-01-16 16:32:32 +0100

    add missing header declaration

--- include/execution.h
+++ include/execution.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 10:17:31 by averin            #+#    #+#             */
-/*   Updated: 2024/01/16 13:53:17 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/16 16:25:58 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ typedef struct s_exec
 
 int		dispatch_cmd(t_cmd *cmd, char **path);
 int		do_exec(t_exec *exec, char **envp);
+char	*find_relative_exec(char *cmd);
+char	*find_path_exec(char *cmd, char **path);
 char	**get_path(void);
 void	free_exec(t_exec exec);
 
