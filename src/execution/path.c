3cfd40a6df468eb09378bcad9962a47c050f4054
averin <averin@student.42.fr>
2024-01-16 13:53:36 +0100

    add get_path

--- src/execution/path.c
+++ src/execution/path.c
@@ -6,8 +6,36 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 10:42:17 by averin            #+#    #+#             */
-/*   Updated: 2024/01/16 10:42:26 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/16 13:53:29 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
+
+char	**get_path(void)
+{
+	size_t	i;
+	char	*path;
+	char	*temp;
+	char	**result;
+
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+	result = ft_split(path, ':');
+	if (!result)
+		return (NULL);
+	i = -1;
+	while (result[++i])
+	{
+		if (result[i][ft_strlen(result[i]) - 1] != '/')
+		{
+			temp = ft_strjoin(result[i], "/");
+			if (!temp)
+				return (ft_fsplit(result), NULL);
+			free(result[i]);
+			result[i] = temp;
+		}
+	}
+	return (result);
+}
