97fcaa46ed4695ee5e813b895d417078dd62e770
averin <averin@student.42.fr>
2024-01-19 14:01:41 +0100

    add ft_strrplc function

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 09:55:16 by averin            #+#    #+#             */
-/*   Updated: 2024/01/04 23:47:03 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/19 13:13:31 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strcut(char *str, char delimiter);
+char	*ft_strrplc(char *from, char *td, char *tr);
 
 int		ft_atoi(const char *nptr);
 long	ft_atol(const char *nptr);
--- libft/src/ft_strrplc.c
+++ libft/src/ft_strrplc.c
@@ -6,9 +6,36 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:05:44 by averin            #+#    #+#             */
-/*   Updated: 2024/01/19 13:08:51 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/19 14:03:14 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * Replace the string `td` in the string `from` by the string `tr`
+ * @param from the string in wich replace
+ * @param td the string to delete
+ * @param tr the string to replace by
+ * @return a new freeable string or NULL
+*/
+char	*ft_strrplc(char *from, char *td, char *tr)
+{
+	size_t	i;
+	size_t	td_len;
+	char	*str;
+
+	td_len = ft_strlen(td);
+	i = -1;
+	str = ft_calloc(ft_strlen(from) - td_len + ft_strlen(tr) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (*from && ft_strncmp(from, td, ft_strlen(td)))
+		str[++i] = *(from++);
+	while (*tr)
+		str[++i] = *(tr++);
+	from += td_len;
+	while (*from)
+		str[++i] = *(from++);
+	return (str);
+}
