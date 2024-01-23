cd55c46610376f5957cf3e7e780f1362460b3a01
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-23 11:41:24 +0100

    add dispatch_cmd call to main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:18:12 by averin            #+#    #+#             */
-/*   Updated: 2024/01/23 09:15:32 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/23 11:40:37 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
+	t_cmd	*cmd;
+	char	**path;
 
-	(void)argc;
-	(void)argv;
+	((void)argc, (void)argv);
 	line = NULL;
+	path = get_path();
+	if (!path)
+		return (NULL);
 	register_signals();
 	while (prompt(&line))
-		;
+		dispatch_cmd(cmd, path, envp);
+	free_cmd(cmd);
+	ft_fsplit(path);
 	return (0);
 }
