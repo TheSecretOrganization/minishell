969f563c2056970fc728fbe57e82a4db027f76fd
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-24 09:43:26 +0100

    create first two struct

--- src/parsing/check_syntax.c
+++ src/parsing/check_syntax.c
@@ -6,10 +6,11 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 15:54:29 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/23 11:09:32 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/24 09:41:58 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 #include "parsing.h"
 
 /**
@@ -36,6 +37,54 @@ t_code	error_syntax(t_code code, char *el, size_t n)
 }
 
 /**
+ * @brief check if the line contains only chars from a set
+ *
+ * @param line current position in line parsed
+ * @param start start of line parsed
+ * @param set set of special chars to check
+ * @return t_bool
+ */
+static t_bool	check_only_set(char *line, char *start, char *set)
+{
+	t_bool	checks[2];
+	size_t	i;
+
+	checks[0] = B_TRUE;
+	checks[1] = B_TRUE;
+	i = -1;
+	while (line[++i] && checks[0] == B_TRUE)
+		if (!ft_strchr(set, line[i]))
+			checks[0] = B_FALSE;
+	if (start)
+	{
+		while (start != line && checks[1] == B_TRUE)
+			if (!ft_strchr(set, *(line++)))
+				checks[1] = B_FALSE;
+		if (checks[0] == B_FALSE && checks[1] == B_FALSE)
+			return (B_FALSE);
+		else
+			return (B_TRUE);
+	}
+	return (checks[0]);
+}
+
+/**
+ * @brief Check bash special chars syntax
+ *
+ * @param line pointer on the current position in the line parsed
+ * @param start start of the line parsed
+ * @return t_code C_SUCCES or an error
+ */
+static t_code	check_special_chars(char **line, char *start)
+{
+	if (ft_strchr(CH_DIR, **line) && check_dir(line))
+		return (C_BAD_USE);
+	else if (ft_strchr(CH_OPE, **line) && check_ope(line, start))
+		return (C_BAD_USE);
+	return (C_SUCCESS);
+}
+
+/**
  * Check bash syntax in one line
  * @param line line to check
  * @return t_code C_SUCCESS or C_BAD_USE
@@ -44,9 +93,11 @@ t_code	check_syntax(char *line)
 {
 	size_t	nq;
 	size_t	nd;
+	char	*start;
 
 	nq = 0;
 	nd = 0;
+	start = line;
 	while (line && *line)
 	{
 		if (*line == '\'' && !(nd % 2))
@@ -55,6 +106,8 @@ t_code	check_syntax(char *line)
 			nd++;
 		if (!(nq % 2) && !(nd % 2) && ft_strchr(CH_ERR, *line))
 			return (error_syntax(C_BAD_USE, line, 1));
+		if (check_special_chars(&line, start))
+			return (C_BAD_USE);
 		line++;
 	}
 	if (nq % 2)
