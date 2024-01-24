2cae1b70258f03e5a89153d3073960777baa4327
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-24 09:42:50 +0100

    create CH_SPCL

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/22 09:47:23 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/24 09:42:33 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define CH_ERR "~`#()\\[]{};?!"
 # define CH_OPE "&|"
 # define CH_DIR "<>"
+# define CH_SPCL "<>&|"
 
 t_code	check_syntax(char *line);
 t_code	clean_memory(t_cmd *cmd, char *line, char *args);
@@ -26,6 +27,5 @@ t_code	error_syntax(t_code code, char *el, size_t n);
 char	*find_next_sep(char *line);
 char	*fspace_njoin(char *s1, char *s2, size_t n);
 t_code	parse_line(t_cmd **cmd, char *line);
-void	print_ats(t_cmd *cmd);
 
 #endif
