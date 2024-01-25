5b65d822ecc69093d648433e6768932c09413022
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-01-25 13:04:00 +0100

    add_dir functions created

--- src/parsing/add_dir.c
+++ src/parsing/add_dir.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:18:39 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/25 11:27:27 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/01/25 13:02:56 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,20 @@
  */
 static char	*get_next_substr(t_ast *ast, char *line)
 {
-	char	*tmp;
+	char	*sub;
+	char	*new_next;
 
-	tmp = find_next_sep(ast->next);
-	while (tmp - ast->next == 1)
+	new_next = find_next_sep(ast->next + 1);
+	while (new_next - ast->next == 1)
 	{
-		ast->next = tmp;
-		tmp = find_next_sep(ast->next);
+		ast->next = new_next;
+		new_next = find_next_sep(ast->next + 1);
 	}
-	tmp = ft_substr(line, ast->next - &(line[0]), tmp - ast->next);
-	if (!tmp)
+	sub = ft_substr(line, (ast->next + 1) - \
+	&(line[0]), new_next - (ast->next + 1));
+	if (!sub)
 		return (error(C_MEM, "ft_substr", M_MEM), NULL);
-	return (tmp);
+	return (ast->next = new_next, sub);
 }
 
 /**
@@ -79,7 +81,7 @@ static t_code	add_out(t_ast *ast, char *line, t_outtype type)
 	file->filename = get_next_substr(ast, line);
 	if (!file->filename)
 		return (free(file), C_MEM);
-	if (addback_cmd(ast->target, new_element(T_INFILE, file)))
+	if (addback_cmd(ast->target, new_element(T_OUTFILE, file)))
 		return (free(file->filename), free(file), \
 		error(C_MEM, "addback_cmd", M_MEM));
 	return (ast->i = ast->next - &(line[0]), C_SUCCESS);
