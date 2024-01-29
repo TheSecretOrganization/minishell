3d130e67cf8034cd206cae4e81aded150fdcbdd1
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-29 09:31:31 +0100

    add exec_builtin

--- src/execution/builtin.c
+++ src/execution/builtin.c
@@ -6,10 +6,22 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 08:13:56 by averin            #+#    #+#             */
-/*   Updated: 2024/01/29 08:14:06 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/29 09:31:19 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
+static void	check_files(t_exec *exec)
+{
+	if (exec->infile == -1)
+		exec->infile = 1;
+	if (exec->outfile == -1)
+		exec->outfile = 0;
+}
 
+int	exec_builtin(t_exec *exec)
+{
+	check_files(exec);
+	return (exec->builtin(exec));
+}
