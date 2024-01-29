46015e3c71781179bee9d3fa5ebaced082c4c067
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-29 09:09:41 +0100

    add builtin commands prototypes

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 08:55:29 by averin            #+#    #+#             */
-/*   Updated: 2024/01/29 08:58:05 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/29 09:09:18 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 
 # include "minishell.h"
 # include "execution.h"
+
+int	cmd_echo(t_exec *exec);
+int	cmd_cd(t_exec *exec);
+int	cmd_pwd(t_exec *exec);
+int	cmd_export(t_exec *exec);
+int	cmd_unset(t_exec *exec);
+int cmd_env(t_exec *exec);
+int	cmd_exit(t_exec *exec);
 
 #endif
