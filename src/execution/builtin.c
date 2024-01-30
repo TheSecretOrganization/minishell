32e4a981361768f4fcedf20c7925b5c6220213ba
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-30 09:30:35 +0100

    replace default in and out file for builtin

--- src/execution/builtin.c
+++ src/execution/builtin.c
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 08:13:56 by averin            #+#    #+#             */
-/*   Updated: 2024/01/29 09:31:19 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/30 09:28:36 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 static void	check_files(t_exec *exec)
 {
 	if (exec->infile == -1)
-		exec->infile = 1;
+		exec->infile = STDIN_FILENO;
 	if (exec->outfile == -1)
-		exec->outfile = 0;
+		exec->outfile = STDOUT_FILENO;
 }
 
 int	exec_builtin(t_exec *exec)
