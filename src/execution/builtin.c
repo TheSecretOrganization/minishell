22ba11ce8406672b8c79c07d840d97aaad8239be
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-30 09:31:31 +0100

    close fd for builtin

--- src/execution/builtin.c
+++ src/execution/builtin.c
@@ -20,8 +20,20 @@ static void	check_files(t_exec *exec)
 		exec->outfile = STDOUT_FILENO;
 }
 
+static void	close_fds(t_exec *exec)
+{
+	if (exec->infile != STDIN_FILENO)
+		(close(exec->infile), exec->infile = -1);
+	if (exec->outfile != STDOUT_FILENO)
+		(close(exec->outfile), exec->outfile = -1);
+}
+
 int	exec_builtin(t_exec *exec)
 {
+	int	code;
+
 	check_files(exec);
-	return (exec->builtin(exec));
+	code = exec->builtin(exec);
+	close_fds(exec);
+	return (code);
 }
