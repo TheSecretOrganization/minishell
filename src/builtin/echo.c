d206252b2017dbc47bed93385ac86754432dcc24
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-30 09:57:31 +0100

    add echo command

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 09:38:16 by averin            #+#    #+#             */
-/*   Updated: 2024/01/29 09:41:45 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/30 09:56:52 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 int	cmd_echo(t_exec *exec)
 {
-	(void) exec;
+	size_t	i;
+	int		first;
+
+	i = 0;
+	first = 1;
+	if (exec->args[0] == NULL)
+		return (C_SUCCESS);
+	if (ft_strncmp("-n", exec->args[1], 3) == 0)
+		i++;
+	while (exec->args[++i])
+	{
+		if (!first)
+			ft_putstr_fd(" ", exec->outfile);
+		ft_putstr_fd(exec->args[i], exec->outfile);
+		first = 0;
+	}
+	if (ft_strncmp("-n", exec->args[1], 3) != 0)
+		ft_putstr_fd("\n", exec->outfile);
 	return (C_SUCCESS);
 }
