4847c1653b6c0d15663f3fcd69477dbe3c8c33ec
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-01-31 10:16:05 +0100

    add pwd command

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,14 +6,35 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 09:38:08 by averin            #+#    #+#             */
-/*   Updated: 2024/01/29 09:42:04 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/31 10:15:07 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+#define SIZE_STEP 100
 
 int	cmd_pwd(t_exec *exec)
 {
-	(void) exec;
-	return (C_SUCCESS);
+	char	*cwd;
+	size_t	size;
+
+	if (exec->args[1] != NULL)
+		return (ft_putstr_fd("pwd: too many arguments\n", 2), C_GEN);
+	cwd = NULL;
+	size = 0;
+	while (!cwd)
+	{
+		size += SIZE_STEP;
+		cwd = ft_calloc(size, sizeof(char));
+		if (!cwd)
+			return (C_MEM);
+		if (!getcwd(cwd, size))
+		{
+			if (errno == ERANGE)
+				(free(cwd), cwd = NULL);
+			else
+				return (free(cwd), perror("getcwd"), C_GEN);
+		}
+	}
+	return (printf("%s\n", cwd), free(cwd), C_SUCCESS);
 }
