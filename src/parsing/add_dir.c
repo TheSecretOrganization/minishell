e58190a0fb91a33046d1980e72a1070ed8eefd82
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-01 20:34:23 +0100

    else if correction

--- src/parsing/add_dir.c
+++ src/parsing/add_dir.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:18:39 by abasdere          #+#    #+#             */
-/*   Updated: 2024/02/01 20:30:24 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/01 20:32:23 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ t_code	add_dir(t_ast *ast, char *line)
 	{
 		if (line[ast->i + 1] == '<')
 			return (ast->i++, add_in(ast, line, IT_HERE_DOC));
-		if (line[ast->i + 1] == '>')
+		else if (line[ast->i + 1] == '>')
 		{
 			tmp = ++(ast->i);
 			if (add_in(ast, line, IT_HERE_DOC))
