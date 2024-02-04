6f06491311757474366bdeb31de1236aff4fe2a2
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-04 15:11:12 +0100

    dev

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 08:55:29 by averin            #+#    #+#             */
-/*   Updated: 2024/02/01 22:42:17 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/03 21:35:33 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	cmd_echo(t_exec *exec);
 int	cmd_cd(t_exec *exec);
 int	cmd_pwd(t_exec *exec);
 int	cmd_export(t_exec *exec);
+int	print_export(char **envp);
+int	remove_quotes(char **arg);
 int	cmd_unset(t_exec *exec);
 int	cmd_env(t_exec *exec);
 int	cmd_exit(t_exec *exec);
--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,60 +6,39 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 09:38:12 by averin            #+#    #+#             */
-/*   Updated: 2024/02/02 18:42:01 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/03 21:45:57 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-static void	swap(char **s1, char **s2)
+static int	process_arg(char *arg, t_data *data)
 {
-	char	*tmp;
+	char	*value;
+	int		code;
 
-	tmp = *s1;
-	*s1 = *s2;
-	*s2 = tmp;
-}
-
-static void	quicksort(char **envp, size_t len)
-{
-	size_t	i;
-	size_t	piv;
-
-	i = -1;
-	piv = 0;
-	if (len <= 1)
-		return ;
-	while (++i < len)
-		if (ft_strncmp(envp[i], envp[len -1], ft_strlen(envp[i])) < 0)
-			swap(envp + i, envp + piv++);
-	swap(envp + piv, envp + len - 1);
-	quicksort(envp, piv++);
-	quicksort(envp + piv, len - piv);
-}
-
-static t_code	print_export(char **envp)
-{
-	t_data	cpy;
-	size_t	i;
-
-	if (cpy_envp(&cpy, envp))
+	if (remove_quotes(&arg))
 		return (C_MEM);
-	i = 0;
-	while (cpy.envp[i])
-		i++;
-	quicksort(cpy.envp, i);
-	i = 0;
-	while (cpy.envp[i])
-		printf("export %s\n", cpy.envp[i++]);
-	return (ft_fsplit(cpy.envp), C_SUCCESS);
+	value = ft_strchr(arg, '=');
+	if (value)
+		*(value++) = '\0';
+	code = ft_setenv(data, arg, value);
+	if (value)
+		*(--value) = '=';
+	return (code);
 }
 
 int	cmd_export(t_exec *exec)
 {
+	size_t	i;
+
 	if (exec->args[0] == NULL)
 		return (C_SUCCESS);
 	else if (exec->args[1] == NULL)
 		return (print_export(exec->data->envp));
+	i = 0;
+	while (exec->args[++i])
+		if (process_arg(exec->args[i], exec->data))
+			return (C_MEM);
 	return (C_SUCCESS);
 }
