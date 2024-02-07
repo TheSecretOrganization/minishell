869ff6f1837747a6357afa21d411546daad4d871
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-07 14:44:50 +0100

    updated unset to update PATH if needed

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 09:38:04 by averin            #+#    #+#             */
-/*   Updated: 2024/02/06 15:47:25 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/07 14:41:03 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,14 @@ int	cmd_unset(t_exec *exec)
 	while (exec->args[++i])
 	{
 		el = ft_getenv(*(exec->data), exec->args[i]);
-		if (el && ft_unenv(exec->data, exec->args[i]))
+		if (!el)
+			continue ;
+		if (ft_unenv(exec->data, exec->args[i]))
 			return (free(el), C_MEM);
 		free(el);
+		if (!ft_strncmp(exec->args[i], "PATH\0", ft_strlen(exec->args[i]) + 1))
+			if (get_path(exec->data))
+				return (C_MEM);
 	}
 	return (C_SUCCESS);
 }
