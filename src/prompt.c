fc8df74d2ab9b8f590a4e5bea199cf35738381db
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-02-08 10:47:43 +0100

    update prompt to bind redraw prompt handler

--- src/prompt.c
+++ src/prompt.c
@@ -6,12 +6,21 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:50:14 by averin            #+#    #+#             */
-/*   Updated: 2024/02/08 10:10:23 by averin           ###   ########.fr       */
+/*   Updated: 2024/02/08 10:46:32 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "colors.h"
+
+static void	handle_sigint(int signal)
+{
+	(void)signal;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
 /**
  * Send a prompt to the user
@@ -20,10 +29,22 @@
 */
 char	*prompt(t_data *data)
 {
+	struct sigaction	old;
+	struct sigaction	saction;
+	sigset_t			set;
+
+	sigemptyset(&set);
+	saction.sa_handler = handle_sigint;
+	saction.sa_mask = set;
+	saction.sa_flags = 0;
+	if (sigaction(SIGINT, &saction, &old) == -1)
+		return (perror("sigaction"), NULL);
 	if (data->line)
 		free(data->line);
 	data->line = readline(PROMPT_HEAD" "WHITE"❯ "DEFAULT);
 	if (ft_strncmp(data->line, "", 1) != 0)
 		add_history(data->line);
+	if (sigaction(SIGINT, &old, NULL) == -1)
+		return (perror("sigaction"), free(line), NULL);
 	return (data->line);
 }
