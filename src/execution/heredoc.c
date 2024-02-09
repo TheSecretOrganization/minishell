8e757aedaca58d9ae99761e8a8e68416e84abec5
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-09 10:46:49 +0100

    expand here_doc managed

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -3,45 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 10:44:54 by averin            #+#    #+#             */
-/*   Updated: 2024/02/08 13:25:21 by averin           ###   ########.fr       */
+/*   Updated: 2024/02/09 10:43:01 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "execution.h"
+#include "parsing.h"
 #include <errno.h>
 
-static void read_here_doc(t_exec *exec, char *delimiter, int wfd)
+static int	of_expand_here_doc(char **line, int status)
+{
+	size_t	i;
+
+	i = -1;
+	while ((*line)[++i])
+	{
+		if ((*line)[i] == '$' && (*line)[i + 1] && (*line)[i + 1] != ' ')
+		{
+			if ((*line)[i + 1] == '?')
+				(*line) = expand_status((*line), status);
+			else
+				(*line) = expand_variable((*line), &i);
+			if (!(*line))
+				return (C_MEM);
+		}
+	}
+	return (C_SUCCESS);
+}
+
+static void	free_here_doc(t_exec *exec, char *delimiter, char *line, int wfd)
+{
+	(free(delimiter), delimiter = NULL);
+	(free(line), line = NULL);
+	(close(wfd), wfd = -1);
+	clean_data(exec->data);
+	ft_fsplit(exec->data->envp);
+	ft_fsplit(exec->args);
+}
+
+static void	read_here_doc(t_exec *exec, char *delimiter, int wfd)
 {
 	char	*line;
 	size_t	len;
 	int		db;
+	int		expand;
 
 	db = delimiter[0] == '"' || delimiter[0] == '\'';
+	expand = 1;
 	if (db)
 	{
 		line = (char [2]){delimiter[0], 0};
-		delimiter = ft_strtrim(delimiter, line);
+		delimiter = ft_fstrtrim(delimiter, line);
 		line = NULL;
+		expand = 0;
 		if (!delimiter)
-			return ;
+			(free_here_doc(exec, delimiter, line, wfd), exit(C_MEM));
 	}
 	line = readline("here_doc > ");
 	len = ft_strlen(delimiter);
 	while (line && ft_strncmp(delimiter, line, len))
+	{
+		if (expand && of_expand_here_doc(&line, exec->data->status))
+			(free_here_doc(exec, delimiter, line, wfd), exit(C_MEM));
 		(ft_putendl_fd(line, wfd), free(line), line = readline("here_doc > "));
-	close(wfd);
-	(clean_data(exec->data), ft_fsplit(exec->data->envp), ft_fsplit(exec->args),
-		free(line), exit(C_SUCCESS));
+	}
+	(free_here_doc(exec, delimiter, line, wfd), exit(C_SUCCESS));
 }
 
 static int	here_doc_prompt(t_exec *exec, char *delimiter, int wfd, int rfd)
 {
 	pid_t	pid;
 	int		code;
+	char	*cpy;
 
 	pid = fork();
 	code = C_SUCCESS;
@@ -51,8 +88,11 @@ static int	here_doc_prompt(t_exec *exec, char *delimiter, int wfd, int rfd)
 	{
 		close(rfd);
 		if (setup_here_doc(exec))
-			exit(C_GEN);
-		read_here_doc(exec, delimiter, wfd);
+			(free_here_doc(exec, NULL, NULL, wfd), exit(C_GEN));
+		cpy = ft_strdup(delimiter);
+		if (!cpy)
+			(free_here_doc(exec, NULL, NULL, wfd), exit(C_MEM));
+		read_here_doc(exec, cpy, wfd);
 	}
 	else
 		wait(&code);
