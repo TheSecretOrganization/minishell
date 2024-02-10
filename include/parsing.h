8010c958c9014cfd0825ac411316e9ba7ff0ec11
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-10 12:23:17 +0100

    updated parsing.h

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:06:06 by abasdere          #+#    #+#             */
-/*   Updated: 2024/02/09 10:02:58 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/10 00:06:07 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,39 +22,33 @@
 
 typedef struct s_ast
 {
-	char	*j_args;
 	char	*next;
+	char	*new_i;
 	t_cmd	*target;
 	size_t	i;
 }	t_ast;
 
 t_code	parse_line(t_data *data);
 
-t_code	remove_quotes(t_data *data, int no_expand);
-t_code	check_quotes(t_data *data);
-t_code	check_syntax(t_data *data);
-t_code	error_syntax(t_code code, char *el, size_t n);
 
-t_code	check_ampersand(char *line, size_t *pos);
-t_code	check_pipe(char *line, size_t *pos, t_bool force_err);
+t_code	error_syntax(t_code code, char *el, size_t n);
+t_code	check_syntax(t_data *data);
+t_code	check_quotes(t_data *data);
+t_code	check_spcl_chars(char *line);
 t_code	check_in(char *line, size_t *pos, t_bool force_err);
 t_code	check_out(char *line, size_t *pos, t_bool force_err);
-t_code	check_spcl_chars(char *line);
 
+t_code	expand(t_data *data);
 char	*expand_variable(char *line, size_t *i);
 char	*expand_status(char *line, int status);
 t_code	expand_var(char **line, size_t *i, int status, size_t nd);
 
 t_code	create_ast(t_data *data);
-
-t_code	add_ope(t_ast *ast, char *line);
 t_code	o_init_cmd(t_cmd **cmd);
-
+t_code	add_ope(t_ast *ast, char *line);
 t_code	add_dir(t_ast *ast, char *line);
-
-t_code	join_args(t_ast *ast, char *line);
-t_code	split_args(t_ast *ast);
-char	*find_next_sep(char *line);
-char	*find_next_arg(char *line, char **end);
+char	*get_next_substr(t_ast *ast, char *line);
+t_code	add_arg(t_ast *ast, char *line);
+void	remove_quotes(char **s);
 
 #endif
