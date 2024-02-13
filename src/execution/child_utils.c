e95c4651906279c0f9dcf505d62d28e62b714033
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-02-13 10:50:11 +0100

    add newline when sigint while waiting for child #219

--- src/execution/child_utils.c
+++ src/execution/child_utils.c
@@ -6,11 +6,17 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 15:12:45 by averin            #+#    #+#             */
-/*   Updated: 2024/02/11 16:20:12 by averin           ###   ########.fr       */
+/*   Updated: 2024/02/13 10:48:27 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
+
+static void	handle_sigint(int signal)
+{
+	g_signal = signal;
+	printf("\n");
+}
 
 /**
  * Wait for all children
@@ -19,9 +25,11 @@
 */
 int	wait_children(int pid)
 {
-	int	wstatus;
-	int	code;
+	int					wstatus;
+	int					code;
+	struct	sigaction	old;
 
+	register_action(SIGINT, &old, &handle_sigint);
 	while (errno != ECHILD)
 	{
 		if (wait(&wstatus) == pid)
@@ -34,5 +42,6 @@ int	wait_children(int pid)
 				printf("Quit\n");
 		}
 	}
+	sigaction(SIGINT, &old, NULL);
 	return (code);
 }
