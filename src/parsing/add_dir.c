230ac98657fbe36b38fc0b7bfa8972f468c6fdd1
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-15 13:37:24 +0100

    fixed type in add_out

--- src/parsing/add_dir.c
+++ src/parsing/add_dir.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:18:39 by abasdere          #+#    #+#             */
-/*   Updated: 2024/02/15 13:16:45 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/15 13:36:52 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,12 @@ static t_code	add_out(t_ast *ast, t_outtype type)
 	t_outfile	*file;
 
 	free(ast->args[ast->i++]);
-	file = ft_calloc(1, sizeof(t_infile));
+	file = ft_calloc(1, sizeof(t_outfile));
 	if (!file)
 		return (free_args(ast), error(C_MEM, "ft_calloc", M_MEM));
 	file->outtype = type;
 	(remove_quotes(ast->args[ast->i]), file->filename = ast->args[ast->i]);
-	if (addback_cmd(ast->target, new_element(T_INFILE, file)) && ast->i++)
+	if (addback_cmd(ast->target, new_element(T_OUTFILE, file)) && ast->i++)
 		return (free_args(ast), free(file->filename),
 			free(file), error(C_MEM, "addback_cmd", M_MEM));
 	return (C_SUCCESS);
